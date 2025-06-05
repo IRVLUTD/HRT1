9bb99890461dc9d981e8bcfbf8059a42e805e4cd
jishnu.p@cs260569.utdallas.edu <jishnu.p@utdallas.edu>
2025-06-04 23:56:53 -0500

    visualize both obj prompts in same image for dual obj scenes

--- vie/run_dual_object_pose_estimation.py
+++ vie/run_dual_object_pose_estimation.py
@@ -2,7 +2,8 @@
 import os
 import sys
 import cv2
-import time  # Added this import
+import time
+import torch
 import rospy
 import argparse
 import numpy as np
@@ -11,7 +12,16 @@ from PIL import Image as PILImg
 from sensor_msgs.msg import Image as RosImage
 from ros_listener import ImageListener
 from bundlesdf_processor import BundleSDFProcessor
-from utils import read_obj_prompts, prettify_prompt, create_symlink, remove_symlink_only, copy_file_if_exists, create_required_out_folders
+from utils import (
+    read_obj_prompts,
+    prettify_prompt,
+    create_symlink,
+    remove_symlink_only,
+    copy_file_if_exists,
+    create_required_out_folders,
+)
+from robokit.utils import combine_masks, annotate, overlay_masks
+
 
 def make_yaml_safe(data):
     """Recursively convert NumPy types and other non-YAML-safe types to Python built-in types."""
@@ -33,10 +43,11 @@ def make_yaml_safe(data):
         # Handle other types by converting to string as a fallback
         return str(data)
 
+
 def print_object_count(objx, prompt, mask):
     """
     Print formatted output with object count, colored red if > 1, green otherwise.
-    
+
     Args:
         objx: Object identifier (e.g., index or name)
         prompt: Text prompt to be prettified
@@ -46,29 +57,73 @@ def print_object_count(objx, prompt, mask):
     num_objects = len(np.unique(mask)) - 1
     color_code = "\033[31m" if num_objects > 1 else "\033[32m"
     reset_code = "\033[0m"
-    colored_num = f"{color_code}{num_objects}{reset_code}"
-    print("{:<10} | {:<20} | {:>15}".format(objx, prettify_prompt(prompt), colored_num))
+    print(
+        f"{color_code}{objx:<10}{reset_code} | {color_code}{prettify_prompt(prompt):<20}{reset_code} | {color_code}{num_objects:>15}{reset_code}"
+    )
+
+
+def filter_phrases(phrases):
+    # print("Filter non null phrases and masks")
+    non_null_phrase_ids = []
+    for phrase_idx, phrase in enumerate(phrases):
+        if phrase != "":
+            non_null_phrase_ids.append(phrase_idx)
+    return non_null_phrase_ids
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--mode", type=str, default="run_video", help="run_video/global_refine/draw_pose")
-    parser.add_argument("--task_dir", type=str, required=True, help="Directory containing task data")
-    parser.add_argument("--video_dir", type=str, default="realworld", help="Directory for video data")
-    parser.add_argument("--out_folder", type=str, default="realworld/out/bundlesdf/rollout", help="Output folder")
-    parser.add_argument("--use_segmenter", type=int, default=0, help="Use segmenter (0 or 1)")
+    parser.add_argument(
+        "--mode",
+        type=str,
+        default="run_video",
+        help="run_video/global_refine/draw_pose",
+    )
+    parser.add_argument(
+        "--task_dir", type=str, required=True, help="Directory containing task data"
+    )
+    parser.add_argument(
+        "--video_dir", type=str, default="realworld", help="Directory for video data"
+    )
+    parser.add_argument(
+        "--out_folder",
+        type=str,
+        default="realworld/out/bundlesdf/rollout",
+        help="Output folder",
+    )
+    parser.add_argument(
+        "--use_segmenter", type=int, default=0, help="Use segmenter (0 or 1)"
+    )
     parser.add_argument("--use_gui", type=int, default=0, help="Use GUI (0 or 1)")
     parser.add_argument("--stride", type=int, default=1, help="Frame interval")
     parser.add_argument("--debug_level", type=int, default=0, help="Debug level")
-    parser.add_argument("--demo_frames", type=int, default=15, help="Number of frames to collect")
-    parser.add_argument("--rollout_frames", type=int, default=5, help="Number of frames to collect")
-    parser.add_argument("--camera", type=str, default="Fetch", help="Camera type (Fetch, Realsense, Azure, etc.)")
+    parser.add_argument(
+        "--demo_frames", type=int, default=15, help="Number of frames to collect"
+    )
+    parser.add_argument(
+        "--rollout_frames", type=int, default=5, help="Number of frames to collect"
+    )
+    parser.add_argument(
+        "--camera",
+        type=str,
+        default="Fetch",
+        help="Camera type (Fetch, Realsense, Azure, etc.)",
+    )
     args = parser.parse_args()
 
     rospy.init_node("merged_bundlesdf_node", anonymous=True)
-    pub_row0_rgb = rospy.Publisher("/bundleSDF/src_rgb_with_pose", RosImage, queue_size=10)
-    pub_row0_masked_rgb = rospy.Publisher("/bundleSDF/src_masked_rgb", RosImage, queue_size=10)
-    pub_row1_rgb = rospy.Publisher("/bundleSDF/dst_rgb_with_pose", RosImage, queue_size=10)
-    pub_row1_masked_rgb = rospy.Publisher("/bundleSDF/dst_masked_rgb", RosImage, queue_size=10)
+    pub_row0_rgb = rospy.Publisher(
+        "/bundleSDF/src_rgb_with_pose", RosImage, queue_size=10
+    )
+    pub_row0_masked_rgb = rospy.Publisher(
+        "/bundleSDF/src_masked_rgb", RosImage, queue_size=10
+    )
+    pub_row1_rgb = rospy.Publisher(
+        "/bundleSDF/dst_rgb_with_pose", RosImage, queue_size=10
+    )
+    pub_row1_masked_rgb = rospy.Publisher(
+        "/bundleSDF/dst_masked_rgb", RosImage, queue_size=10
+    )
 
     root_dir = os.path.normpath(args.task_dir)
     realworld_dir = args.video_dir
@@ -95,18 +150,26 @@ def main():
         realworld_depth_dir = os.path.join(realworld_dir, "depth")
         realworld_pose_dir = os.path.join(realworld_dir, "pose")
         realworld_mask_dir = os.path.join(realworld_dir, f"masks_{objx}")
-        realworld_bsdf_demo_dir = os.path.join(realworld_dir, "out/bundlesdf/demonstration", objx)
+        realworld_bsdf_demo_dir = os.path.join(
+            realworld_dir, "out/bundlesdf/demonstration", objx
+        )
         realworld_bsdf_oic_dir = os.path.join(realworld_bsdf_demo_dir, "ob_in_cam")
-        realworld_bsdf_pose_overlay_dir = os.path.join(realworld_bsdf_demo_dir, "pose_overlayed_rgb")
+        realworld_bsdf_pose_overlay_dir = os.path.join(
+            realworld_bsdf_demo_dir, "pose_overlayed_rgb"
+        )
         create_required_out_folders(realworld_bsdf_demo_dir)
         cam_k_file_source = os.path.join(root_dir, "cam_K.txt")
         cam_k_file_target = os.path.join(realworld_dir, "cam_K.txt")
         obj_prompt_mapper_src = os.path.join(bsdf_demo_dir, "obj_prompt_mapper.json")
-        obj_prompt_mapper_dst = os.path.join(os.path.dirname(realworld_bsdf_demo_dir), "obj_prompt_mapper.json")
+        obj_prompt_mapper_dst = os.path.join(
+            os.path.dirname(realworld_bsdf_demo_dir), "obj_prompt_mapper.json"
+        )
         copy_file_if_exists(cam_k_file_source, cam_k_file_target)
         copy_file_if_exists(obj_prompt_mapper_src, obj_prompt_mapper_dst)
 
-        rospy.loginfo(f"Collecting {args.demo_frames} frames from source directories...")
+        rospy.loginfo(
+            f"Collecting {args.demo_frames} frames from source directories..."
+        )
         for i in range(args.demo_frames):
             frame_str = f"{i:06d}"
             files = [
@@ -114,11 +177,23 @@ def main():
                 (depth_source_dir, realworld_depth_dir, f"{frame_str}.png", "Depth"),
                 (mask_source_dir, realworld_mask_dir, f"{frame_str}.png", "Mask"),
                 (pose_source_dir, realworld_pose_dir, f"{frame_str}.npz", "Pose"),
-                (os.path.join(bsdf_demo_dir, objx, "ob_in_cam"), realworld_bsdf_oic_dir, f"{frame_str}.txt", "obj in camera pose txt"),
-                (os.path.join(bsdf_demo_dir, objx, "pose_overlayed_rgb"), realworld_bsdf_pose_overlay_dir, f"{frame_str}.png", "BSDF Pose Overlayed RGB"),
+                (
+                    os.path.join(bsdf_demo_dir, objx, "ob_in_cam"),
+                    realworld_bsdf_oic_dir,
+                    f"{frame_str}.txt",
+                    "obj in camera pose txt",
+                ),
+                (
+                    os.path.join(bsdf_demo_dir, objx, "pose_overlayed_rgb"),
+                    realworld_bsdf_pose_overlay_dir,
+                    f"{frame_str}.png",
+                    "BSDF Pose Overlayed RGB",
+                ),
             ]
             for src_dir, tgt_dir, filename, label in files:
-                copy_file_if_exists(os.path.join(src_dir, filename), os.path.join(tgt_dir, filename))
+                copy_file_if_exists(
+                    os.path.join(src_dir, filename), os.path.join(tgt_dir, filename)
+                )
 
     print("Step-2: Get real-time RGB, depth, masks from robot")
     curr_frame = args.demo_frames
@@ -126,28 +201,50 @@ def main():
     time.sleep(0.1)
     timing_results["mask_prediction"] = {}
 
-    print("\n{:<10} | {:<20} | {:>15}".format("Object ID", "Text Prompt", "Objects Detected"))
+    print(
+        "\n{:<10} | {:<20} | {:>15}".format(
+            "Object ID", "Text Prompt", "Objects Detected"
+        )
+    )
     print("-" * 10 + "|" + "-" * 21 + "|" + "-" * 16)
-    
+
+    # capture one realtime frame to test whether the demo text prompts work
+    # if not, ask user to input custom text prompts and veify in realtime
     obj_prompt_mapper_rollout = obj_prompt_mapper.copy()
-    im_color, depth_img, rgb_frame_id, rgb_frame_stamp, RT_camera, RT_laser = listener.get_latest_listener_data()
+    im_color, depth_img, rgb_frame_id, rgb_frame_stamp, RT_camera, RT_laser = (
+        listener.get_latest_listener_data()
+    )
 
     for objx, prompt in obj_prompt_mapper_rollout.items():
         while True:
             # Generate the mask and annotated image using the current prompt
-            bboxes, phrases, gdino_conf = listener.get_gdino_preds(im_color, prettify_prompt(prompt))
-            
+            bboxes, phrases, gdino_conf = listener.get_gdino_preds(
+                im_color, prettify_prompt(prompt)
+            )
+            non_null_phrase_ids = filter_phrases(phrases)
+            bboxes = bboxes[non_null_phrase_ids]
+            gdino_conf = gdino_conf[non_null_phrase_ids]
             print(bboxes, phrases)
 
             # Ask the user if the current prompt is correct
-            user_input = input(f"Current text prompt is '{prompt}'. Is this correct? (Y/N): ").strip().lower()
-            
+            user_input = (
+                input(f"Current text prompt is '{prompt}'. Is this correct? (Y/N): ")
+                .strip()
+                .lower()
+            )
+
             # Validate user input
-            while user_input not in ['y', 'n']:
+            while user_input not in ["y", "n"]:
                 print("Invalid input. Please enter 'Y' for yes or 'N' for no.")
-                user_input = input(f"Current text prompt is '{prompt}'. Is this correct? (Y/N): ").strip().lower()
-            
-            if user_input == 'y':
+                user_input = (
+                    input(
+                        f"Current text prompt is '{prompt}'. Is this correct? (Y/N): "
+                    )
+                    .strip()
+                    .lower()
+                )
+
+            if user_input == "y":
                 # If the prompt is correct, save it and move on to the next object
                 obj_prompt_mapper_rollout[objx] = prompt
                 break
@@ -156,34 +253,72 @@ def main():
                 prompt = input("Please enter your custom prompt: ").strip()
                 obj_prompt_mapper_rollout[objx] = prompt
 
-
     while (
         not rospy.is_shutdown() and curr_frame <= args.demo_frames + args.rollout_frames
     ):
         try:
-            im_color, depth_img, rgb_frame_id, rgb_frame_stamp, RT_camera, RT_laser = listener.get_latest_listener_data()
+            im_color, depth_img, rgb_frame_id, rgb_frame_stamp, RT_camera, RT_laser = (
+                listener.get_latest_listener_data()
+            )
             if im_color is None:
                 continue
-            # im_color = im_color[:, :, (2, 1, 0)]  # BGR to RGB
+            # im_color = im_color[:, :, (2, 1, 0)]  # BGR to RGB # gazebo camera gives BGR images, for realworld rollout, this is not needed
+            annotated_pil = PILImg.fromarray(im_color.copy())
+            mask_list = []
+            bboxes_list = []
+            phrases_list = []
+            gdino_conf_list = []
             for objx, prompt in obj_prompt_mapper_rollout.items():
-                bbox_annotated_img, mask, mask_time = listener.get_gsam_mask(im_color, prettify_prompt(prompt))
-                listener.publish_overlay(bbox_annotated_img, rgb_frame_stamp, rgb_frame_id)
-                listener.publish_mask(mask, rgb_frame_stamp, rgb_frame_id)
-                print_object_count(objx, prettify_prompt(prompt), mask)
+                img_pil, masks, image_pil_bboxes, gdino_conf, phrases, mask_time = (
+                    listener.get_gsam_mask(im_color, prettify_prompt(prompt))
+                )
+
+                non_null_phrase_ids = filter_phrases(phrases)
+                phrases = [phrases[i] for i in non_null_phrase_ids]
+                image_pil_bboxes = image_pil_bboxes[non_null_phrase_ids]
+                gdino_conf = gdino_conf[non_null_phrase_ids]
+                masks = masks[non_null_phrase_ids]
+
+                mask_list.append(masks)
+                bboxes_list.append(image_pil_bboxes)
+                phrases_list.append(phrases)
+                gdino_conf_list.append(gdino_conf)
+
                 realworld_mask_dir = os.path.join(realworld_dir, f"masks_{objx}")
                 rgb_path = os.path.join(realworld_rgb_dir, f"{curr_frame:06d}.jpg")
                 depth_path = os.path.join(realworld_depth_dir, f"{curr_frame:06d}.png")
                 mask_path = os.path.join(realworld_mask_dir, f"{curr_frame:06d}.png")
                 pose_path = os.path.join(realworld_pose_dir, f"{curr_frame:06d}.npz")
                 cv2.imwrite(depth_path, depth_img)
+                mask = combine_masks(masks[:, 0, :, :]).cpu().numpy()
+                print_object_count(objx, prettify_prompt(prompt), mask)
                 mask[mask > 0] = 255
                 cv2.imwrite(mask_path, mask)
                 PILImg.fromarray(im_color).save(rgb_path)
                 np.savez(pose_path, RT_camera=RT_camera)
-                timing_results["mask_prediction"].setdefault(objx, []).append({"frame": curr_frame, "time": mask_time})
+                timing_results["mask_prediction"].setdefault(objx, []).append(
+                    {"frame": curr_frame, "time": mask_time}
+                )
                 timing_results[f"{objx}_text_prompt"] = prompt
             print("-" * 11 + "*" + "-" * 22 + "*" + "-" * 16)
+
+            # merge the mask predictions for all the prompts and publish at once
+            _masks = torch.cat(mask_list, dim=0)
+            _image_pil_bboxes = torch.cat(bboxes_list, dim=0)
+            _phrases = [phrase for sublist in phrases_list for phrase in sublist]
+            _gdino_conf = np.concatenate(gdino_conf_list, axis=0)
+            annotated_pil = annotate(
+                overlay_masks(annotated_pil, _masks),
+                _image_pil_bboxes,
+                _gdino_conf,
+                _phrases,
+            )
+            listener.publish_overlay(
+                np.array(annotated_pil), rgb_frame_stamp, rgb_frame_id
+            )
+
             curr_frame += 1
+
         except Exception as e:
             print(f"Error in frame {curr_frame}: {e}")
             continue
@@ -195,7 +330,9 @@ def main():
     timing_results["bundlesdf_processing"] = {}
 
     for objx, prompt in obj_prompt_mapper.items():
-        realworld_mask_dir = os.path.abspath(os.path.join(realworld_dir, f"masks_{objx}"))
+        realworld_mask_dir = os.path.abspath(
+            os.path.join(realworld_dir, f"masks_{objx}")
+        )
         create_symlink(realworld_mask_dir, masks_symlink_path)
         print(f"Created symlink: {masks_symlink_path} -> {realworld_mask_dir}")
         processor = BundleSDFProcessor(
@@ -208,7 +345,9 @@ def main():
             K=K,
         )
         try:
-            process_time = processor.process(pub_row0_rgb, pub_row0_masked_rgb, pub_row1_rgb, pub_row1_masked_rgb)
+            process_time = processor.process(
+                pub_row0_rgb, pub_row0_masked_rgb, pub_row1_rgb, pub_row1_masked_rgb
+            )
             rollout_details[objx] = {
                 "out_folder": processor.out_folder,
                 "obj_prompt": prettify_prompt(prompt),
@@ -220,32 +359,33 @@ def main():
             remove_symlink_only(masks_symlink_path)
             print(f"Removed symlink: {masks_symlink_path}")
 
-    rollout_details["K"] = {
-        "rollout": {},
-        "demonstration": {}
-    }
+    rollout_details["K"] = {"rollout": {}, "demonstration": {}}
 
-    rollout_details["K"]["rollout"]["fx"] = float(K[0,0])
-    rollout_details["K"]["rollout"]["fy"] = float(K[1,1])
-    rollout_details["K"]["rollout"]["cx"] = float(K[0,2])
-    rollout_details["K"]["rollout"]["cy"] = float(K[1,2])
+    rollout_details["K"]["rollout"]["fx"] = float(K[0, 0])
+    rollout_details["K"]["rollout"]["fy"] = float(K[1, 1])
+    rollout_details["K"]["rollout"]["cx"] = float(K[0, 2])
+    rollout_details["K"]["rollout"]["cy"] = float(K[1, 2])
     rollout_details["args"] = vars(args)
     rollout_details["timing_results"] = timing_results
 
     cam_K = np.loadtxt(cam_k_file_source)
-    rollout_details["K"]["demonstration"]["fx"] = float(cam_K[0,0])
-    rollout_details["K"]["demonstration"]["fy"] = float(cam_K[1,1])
-    rollout_details["K"]["demonstration"]["cx"] = float(cam_K[0,2])
-    rollout_details["K"]["demonstration"]["cy"] = float(cam_K[1,2])
+    rollout_details["K"]["demonstration"]["fx"] = float(cam_K[0, 0])
+    rollout_details["K"]["demonstration"]["fy"] = float(cam_K[1, 1])
+    rollout_details["K"]["demonstration"]["cx"] = float(cam_K[0, 2])
+    rollout_details["K"]["demonstration"]["cy"] = float(cam_K[1, 2])
 
     print("\nTiming Results:")
     for objx in timing_results["mask_prediction"]:
         times = [t["time"] for t in timing_results["mask_prediction"][objx]]
         avg_time = np.mean(times) if times else 0
-        print(f"Object {objx} Mask Prediction - Avg Time: {avg_time:.3f} seconds, Frames: {len(times)}")
+        print(
+            f"Object {objx} Mask Prediction - Avg Time: {avg_time:.3f} seconds, Frames: {len(times)}"
+        )
         rollout_details[objx]["mask_prediction_time"] = avg_time
     for objx, proc_time in timing_results["bundlesdf_processing"].items():
-        print(f"Object {objx} BundleSDF Processing - Total Time: {proc_time:.3f} seconds")
+        print(
+            f"Object {objx} BundleSDF Processing - Total Time: {proc_time:.3f} seconds"
+        )
         rollout_details[objx]["bundlesdf_processing_time"] = proc_time
 
     # Convert rollout_details to YAML-safe types
