3a27625880e378021a0ff8ac1dd25ca396eb2f2c
jishnu.p@cs260569.utdallas.edu <jishnu.p@utdallas.edu>
2025-06-08 23:50:02 -0500

    fix

---
+++ vie/bsdf/bundlesdf_processor.py
@@ -0,0 +1,235 @@
+# ----------------------------------------------------------------------------------------------------
+# Work done while being at the Intelligent Robotics and Vision Lab at the University of Texas, Dallas
+# Please check the licenses of the respective works utilized here before using this script.
+# 🖋️ Jishnu Jaykumar Padalunkal (2025).
+# ----------------------------------------------------------------------------------------------------
+
+import os
+import sys
+import cv2
+import time
+import rospy
+import shutil
+import numpy as np
+from PIL import Image as PILImg
+from std_msgs.msg import Float64MultiArray
+from sensor_msgs.msg import Image as RosImage
+import ros_numpy as rnp
+
+# Add BundleSDF directory to sys.path
+code_dir = os.path.dirname(os.path.realpath(__file__))
+bundle_sdf_dir = os.path.join(code_dir, "..", "BundleSDF")
+sys.path.append(bundle_sdf_dir)
+try:
+    from bundlesdf import BundleSdf, YcbineoatReader
+    from segmentation_utils import Segmenter
+except ModuleNotFoundError as e:
+    print(f"Error importing BundleSDF modules: {e}")
+    print(
+        "Ensure 'my_cpp' module is built in BundleSDF/build/. Run 'cmake .. && make' in BundleSDF/build/"
+    )
+    sys.exit(1)
+from utils import download_loftr_weights, remove_unecessary_files
+import yaml as pyyaml
+
+
+class BundleSDFProcessor:
+    def __init__(
+        self, video_dir, out_folder, use_segmenter, use_gui, stride, debug_level, K
+    ):
+        self.video_dir = video_dir
+        self.out_folder = out_folder
+        self.use_segmenter = bool(use_segmenter)
+        self.use_gui = bool(use_gui)
+        self.stride = stride
+        self.debug_level = debug_level
+        self.K = K
+        self.bundle_sdf_dir = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "BundleSDF"
+        )
+
+    def configure_bundletrack(self):
+        os.system(f"rm -rf {self.out_folder} && mkdir -p {self.out_folder}")
+        weights_dir = os.path.join(
+            self.bundle_sdf_dir, "BundleTrack", "LoFTR", "weights"
+        )
+        download_loftr_weights(weights_dir)
+        config_path = os.path.join(
+            self.bundle_sdf_dir, "BundleTrack", "config_ho3d.yml"
+        )
+        if not os.path.exists(config_path):
+            raise FileNotFoundError(f"Missing {config_path}")
+        with open(config_path, "r") as f:
+            cfg_bundletrack = pyyaml.safe_load(f)
+        cfg_bundletrack["SPDLOG"] = int(self.debug_level)
+        cfg_bundletrack["depth_processing"]["percentile"] = 95
+        cfg_bundletrack["erode_mask"] = 3
+        cfg_bundletrack["debug_dir"] = f"{self.out_folder}/"
+        cfg_bundletrack["bundle"]["max_BA_frames"] = 10
+        cfg_bundletrack["bundle"]["max_optimized_feature_loss"] = 0.03
+        cfg_bundletrack["feature_corres"]["max_dist_neighbor"] = 0.02
+        cfg_bundletrack["feature_corres"]["max_normal_neighbor"] = 30
+        cfg_bundletrack["feature_corres"]["max_dist_no_neighbor"] = 0.01
+        cfg_bundletrack["feature_corres"]["max_normal_no_neighbor"] = 20
+        cfg_bundletrack["feature_corres"]["map_points"] = True
+        cfg_bundletrack["feature_corres"]["resize"] = 400
+        cfg_bundletrack["feature_corres"]["rematch_after_nerf"] = True
+        cfg_bundletrack["keyframe"]["min_rot"] = 5
+        cfg_bundletrack["ransac"]["inlier_dist"] = 0.01
+        cfg_bundletrack["ransac"]["inlier_normal_angle"] = 20
+        cfg_bundletrack["ransac"]["max_trans_neighbor"] = 0.02
+        cfg_bundletrack["ransac"]["max_rot_deg_neighbor"] = 30
+        cfg_bundletrack["ransac"]["max_trans_no_neighbor"] = 0.01
+        cfg_bundletrack["ransac"]["max_rot_no_neighbor"] = 10
+        cfg_bundletrack["p2p"]["max_dist"] = 0.02
+        cfg_bundletrack["p2p"]["max_normal_angle"] = 45
+        cfg_track_dir = os.path.join(self.out_folder, "config_bundletrack.yml")
+        with open(cfg_track_dir, "w") as f:
+            pyyaml.safe_dump(cfg_bundletrack, f)
+        return cfg_track_dir, cfg_bundletrack
+
+    def configure_nerf(self, cfg_bundletrack):
+        config_path = os.path.join(self.bundle_sdf_dir, "config.yml")
+        if not os.path.exists(config_path):
+            raise FileNotFoundError(f"Missing {config_path}")
+        with open(config_path, "r") as f:
+            cfg_nerf = pyyaml.safe_load(f)
+        cfg_nerf["continual"] = True
+        cfg_nerf["trunc_start"] = 0.01
+        cfg_nerf["trunc"] = 0.01
+        cfg_nerf["mesh_resolution"] = 0.005
+        cfg_nerf["down_scale_ratio"] = 1
+        cfg_nerf["fs_sdf"] = 0.1
+        cfg_nerf["far"] = cfg_bundletrack["depth_processing"]["zfar"]
+        cfg_nerf["datadir"] = (
+            f"{cfg_bundletrack['debug_dir']}/nerf_with_bundletrack_online"
+        )
+        cfg_nerf["notes"] = ""
+        cfg_nerf["expname"] = "nerf_with_bundletrack_online"
+        cfg_nerf["save_dir"] = cfg_nerf["datadir"]
+        cfg_nerf_dir = os.path.join(self.out_folder, "config_nerf.yml")
+        with open(cfg_nerf_dir, "w") as f:
+            pyyaml.safe_dump(cfg_nerf, f)
+        return cfg_nerf_dir, cfg_nerf
+
+    def keep_relevant_files(self):
+        keep = ("ob_in_cam", "pose_overlayed_rgb", "cam_K.txt")
+        for item in os.listdir(self.out_folder):
+            item_path = os.path.join(self.out_folder, item)
+            if item not in keep:
+                if os.path.isdir(item_path):
+                    shutil.rmtree(item_path)
+                else:
+                    os.remove(item_path)
+
+    def process(
+        self, pub_row0_rgb, pub_row0_masked_rgb, pub_row1_rgb, pub_row1_masked_rgb
+    ):
+        start_time = time.time()
+        cfg_track_dir, cfg_bundletrack = self.configure_bundletrack()
+        cfg_nerf_dir, cfg_nerf = self.configure_nerf(cfg_bundletrack)
+        segmenter = Segmenter() if self.use_segmenter else None
+        tracker = BundleSdf(
+            cfg_track_dir=cfg_track_dir,
+            cfg_nerf_dir=cfg_nerf_dir,
+            start_nerf_keyframes=5,
+            use_gui=self.use_gui,
+        )
+        reader = YcbineoatReader(video_dir=self.video_dir, shorter_side=480)
+        is_first_frame = True
+        ob_in_cam = np.eye(4)
+        ob_in_cam_dir = f"{self.out_folder}/ob_in_cam"
+        pose_overlayed_rgb_dir = f"{self.out_folder}/pose_overlayed_rgb"
+        os.makedirs(ob_in_cam_dir, exist_ok=True)
+        os.makedirs(pose_overlayed_rgb_dir, exist_ok=True)
+
+        for i in range(0, len(reader.color_files), self.stride):
+            if rospy.is_shutdown():
+                break
+            color_file = reader.color_files[i]
+            color = cv2.imread(color_file)
+            H0, W0 = color.shape[:2]
+            depth = reader.get_depth(i)
+            H, W = depth.shape[:2]
+            color = cv2.resize(color, (W, H), interpolation=cv2.INTER_NEAREST)
+            depth = cv2.resize(depth, (W, H), interpolation=cv2.INTER_NEAREST)
+            if i == 0:
+                mask = reader.get_mask(0)
+                mask = cv2.resize(mask, (W, H), interpolation=cv2.INTER_NEAREST)
+                if segmenter:
+                    mask = segmenter.run(color_file.replace("rgb", "masks"))
+            else:
+                if segmenter:
+                    mask = segmenter.run(color_file.replace("rgb", "masks"))
+                else:
+                    mask = reader.get_mask(i)
+                    mask = cv2.resize(mask, (W, H), interpolation=cv2.INTER_NEAREST)
+            if cfg_bundletrack["erode_mask"] > 0:
+                kernel = np.ones(
+                    (cfg_bundletrack["erode_mask"], cfg_bundletrack["erode_mask"]),
+                    np.uint8,
+                )
+                mask = cv2.erode(mask.astype(np.uint8), kernel)
+            id_str = reader.id_strs[i]
+            _filename, ob_in_cam, _frames = tracker.run(
+                color,
+                depth,
+                self.K,
+                id_str,
+                mask=mask,
+                occ_mask=None,
+                pose_in_model=ob_in_cam,
+            )
+            # print("\n=================================================================\n")
+            # print(ob_in_cam)
+            # print("\n=================================================================\n")
+            if _frames is not None:
+                try:
+                    if is_first_frame:
+                        row0_rgb = np.array(_frames["row0"]["rgb"])[..., :3]
+                        # row0_masked_rgb = np.array(_frames["row0"]["masked_rgb"])[..., :3]
+                        first_frame_pose = ob_in_cam
+                        is_first_frame = False
+                    row1_rgb = np.array(_frames["row1"]["rgb"])[..., :3]
+                    # row1_masked_rgb = np.array(_frames["row1"]["masked_rgb"])[..., :3]
+                    ros_row0_rgb = rnp.msgify(RosImage, row0_rgb, "rgb8")
+                    # ros_row0_masked_rgb = rnp.msgify(RosImage, row0_masked_rgb, "rgb8")
+                    ros_row1_rgb = rnp.msgify(RosImage, row1_rgb, "rgb8")
+                    # ros_row1_masked_rgb = rnp.msgify(RosImage, row1_masked_rgb, "rgb8")
+                    stamp = rospy.Time.now()
+                    ros_row0_rgb.header.stamp = stamp
+                    ros_row0_rgb.header.frame_id = "camera_frame"
+                    # ros_row0_masked_rgb.header.stamp = stamp
+                    # ros_row0_masked_rgb.header.frame_id = "camera_frame"
+                    ros_row1_rgb.header.stamp = stamp
+                    ros_row1_rgb.header.frame_id = "camera_frame"
+                    # ros_row1_masked_rgb.header.stamp = stamp
+                    # ros_row1_masked_rgb.header.frame_id = "camera_frame"
+                    pub_row0_rgb.publish(ros_row0_rgb)
+                    # pub_row0_masked_rgb.publish(ros_row0_masked_rgb)
+                    pub_row1_rgb.publish(ros_row1_rgb)
+                    # pub_row1_masked_rgb.publish(ros_row1_masked_rgb)
+                    rospy.loginfo(f"Published images for frame {id_str}")
+                except Exception as e:
+                    rospy.logerr(f"Error publishing images: {e}")
+            _filename = _filename.split(".")[0]
+            posetxt_filename = f"{ob_in_cam_dir}/{_filename}.txt"
+            np.savetxt(posetxt_filename, ob_in_cam)
+            PILImg.fromarray(row1_rgb).save(
+                os.path.join(pose_overlayed_rgb_dir, f"{_filename}.png")
+            )
+
+        # cue (jishnu): uncomment to publish the first frame pose and ob_in_cam
+        # pub = rospy.Publisher("/bundleSDF/poses", Float64MultiArray, queue_size=10)
+        # msg = Float64MultiArray()
+        # msg.data = np.concatenate((first_frame_pose.flatten(), ob_in_cam.flatten())).tolist()
+        # for _ in range(5):
+        #     rospy.loginfo("Publishing...")
+        #     pub.publish(msg)
+        #     rospy.sleep(0.1)
+
+        remove_unecessary_files(self.out_folder)
+        tracker.on_finish()
+        self.keep_relevant_files()
+        process_time = time.time() - start_time
+        return process_time
---
+++ vie/robokit/ros/ros_listener.py
@@ -0,0 +1,213 @@
+# ----------------------------------------------------------------------------------------------------
+# Work done while being at the Intelligent Robotics and Vision Lab at the University of Texas, Dallas
+# Please check the licenses of the respective works utilized here before using this script.
+# 🖋️ Jishnu Jaykumar Padalunkal (2025).
+# ----------------------------------------------------------------------------------------------------
+
+import os
+import cv2
+import time
+import rospy
+import threading
+import ros_numpy
+import numpy as np
+import message_filters
+from PIL import Image as PILImg
+from sensor_msgs.msg import Image as RosImage, CameraInfo
+from robokit.ros.ros_utils import ros_qt_to_rt
+import tf2_ros
+from robokit.perception import GroundingDINOObjectPredictor, SegmentAnythingPredictor
+from robokit.utils import annotate, overlay_masks, combine_masks, filter_large_boxes
+from utils import compute_xyz
+
+
+class FetchImageListener:
+    def __init__(self, camera="Fetch"):
+        self.im = None
+        self.depth = None
+        self.rgb_frame_id = None
+        self.rgb_frame_stamp = None
+        self.counter = 0
+        self.trigger_flag = None
+        self.gdino = GroundingDINOObjectPredictor()
+        self.SAM = SegmentAnythingPredictor()
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        self.mask_pub = rospy.Publisher("gsam/mask", RosImage, queue_size=10)
+        self.image_pub = rospy.Publisher("gsam/overlay", RosImage, queue_size=10)
+
+        if camera == "Fetch":
+            self.base_frame = "base_link"
+            rgb_sub = message_filters.Subscriber(
+                "/head_camera/rgb/image_raw", RosImage, queue_size=10
+            )
+            depth_sub = message_filters.Subscriber(
+                "/head_camera/depth_registered/image_raw", RosImage, queue_size=10
+            )
+            msg = rospy.wait_for_message("/head_camera/rgb/camera_info", CameraInfo)
+            self.camera_frame = "head_camera_rgb_optical_frame"
+            self.target_frame = self.base_frame
+        elif camera == "Realsense":
+            self.base_frame = "measured/base_link"
+            rgb_sub = message_filters.Subscriber(
+                "/camera/color/image_raw", RosImage, queue_size=10
+            )
+            depth_sub = message_filters.Subscriber(
+                "/camera/aligned_depth_to_color/image_raw", RosImage, queue_size=10
+            )
+            msg = rospy.wait_for_message("/camera/color/camera_info", CameraInfo)
+            self.camera_frame = "measured/camera_color_optical_frame"
+            self.target_frame = self.base_frame
+        elif camera == "Azure":
+            self.base_frame = "measured/base_link"
+            rgb_sub = message_filters.Subscriber(
+                "/k4a/rgb/image_raw", RosImage, queue_size=10
+            )
+            depth_sub = message_filters.Subscriber(
+                "/k4a/depth_to_rgb/image_raw", RosImage, queue_size=10
+            )
+            msg = rospy.wait_for_message("/k4a/rgb/camera_info", CameraInfo)
+            self.camera_frame = "rgb_camera_link"
+            self.target_frame = self.base_frame
+        else:
+            self.base_frame = f"{camera}_rgb_optical_frame"
+            rgb_sub = message_filters.Subscriber(
+                f"/{camera}/rgb/image_color", RosImage, queue_size=10
+            )
+            depth_sub = message_filters.Subscriber(
+                f"/{camera}/depth_registered/image", RosImage, queue_size=10
+            )
+            msg = rospy.wait_for_message(f"/{camera}/rgb/camera_info", CameraInfo)
+            self.camera_frame = f"{camera}_rgb_optical_frame"
+            self.target_frame = self.base_frame
+
+        self.intrinsics = np.array(msg.K).reshape(3, 3)
+        self.fx = self.intrinsics[0, 0]
+        self.fy = self.intrinsics[1, 1]
+        self.px = self.intrinsics[0, 2]
+        self.py = self.intrinsics[1, 2]
+
+        print("\n=================================================================\n")
+        print(f"Camera intrinsics\n {self.intrinsics}")
+        print("\n=================================================================\n")
+
+        queue_size = 1
+        slop_seconds = 0.3  # to sync rgb and depth images
+        ts = message_filters.ApproximateTimeSynchronizer(
+            [rgb_sub, depth_sub], queue_size, slop_seconds
+        )
+        ts.registerCallback(self.callback_rgbd)
+
+    def callback_rgbd(self, rgb, depth):
+        try:
+            transform = self.tf_buffer.lookup_transform(
+                self.base_frame,
+                self.camera_frame,
+                rospy.Time(0),
+                timeout=rospy.Duration(1.0),
+            )
+            trans = [
+                transform.transform.translation.x,
+                transform.transform.translation.y,
+                transform.transform.translation.z,
+            ]
+            rot = [
+                transform.transform.rotation.x,
+                transform.transform.rotation.y,
+                transform.transform.rotation.z,
+                transform.transform.rotation.w,
+            ]
+            RT_camera = ros_qt_to_rt(rot, trans)
+
+        except (
+            tf2_ros.LookupException,
+            tf2_ros.ConnectivityException,
+            tf2_ros.ExtrapolationException,
+        ) as e:
+            rospy.logwarn(f"Transform lookup failed: {e}")
+            RT_camera = None
+
+        if depth.encoding == "32FC1":
+            depth_cv = ros_numpy.numpify(depth)
+            depth_cv[np.isnan(depth_cv)] = 0
+            depth_cv = depth_cv * 1000
+            depth_cv = depth_cv.astype(np.uint16)
+        elif depth.encoding == "16UC1":
+            depth_cv = ros_numpy.numpify(depth).copy().astype(np.float32)
+            depth_cv /= 1000.0
+        else:
+            rospy.logerr_throttle(1, f"Unsupported depth type: {depth.encoding}")
+            return
+
+        im = ros_numpy.numpify(rgb)
+        with threading.Lock():
+            self.im = im.copy()
+            self.depth = depth_cv.copy()
+            self.rgb_frame_id = rgb.header.frame_id
+            self.rgb_frame_stamp = rgb.header.stamp
+            self.height = depth_cv.shape[0]
+            self.width = depth_cv.shape[1]
+            self.RT_camera = RT_camera
+
+    def get_latest_listener_data(self):
+        with threading.Lock():
+            if self.im is None:
+                return None, None, None, None, None
+            im_color = self.im.copy()
+            depth_img = self.depth.copy()
+            rgb_frame_id = self.rgb_frame_id
+            rgb_frame_stamp = self.rgb_frame_stamp
+            RT_camera = self.RT_camera.copy() if self.RT_camera is not None else None
+            return (
+                im_color,
+                depth_img,
+                rgb_frame_id,
+                rgb_frame_stamp,
+                RT_camera,
+            )
+
+    def get_gdino_preds(self, im_color, text_prompt):
+        im = im_color.astype(np.uint8)
+        img_pil = PILImg.fromarray(im)
+        bboxes, phrases, gdino_conf = self.gdino.predict(img_pil, text_prompt)
+        return bboxes, phrases, gdino_conf
+
+    def get_gsam_mask(self, im_color, text_prompt):
+        start_time = time.time()
+        im = im_color.astype(np.uint8)
+        img_pil = PILImg.fromarray(im)
+        bboxes, phrases, gdino_conf = self.gdino.predict(img_pil, text_prompt)
+        w, h = im.shape[1], im.shape[0]
+        image_pil_bboxes = self.gdino.bbox_to_scaled_xyxy(bboxes, w, h)
+        image_pil_bboxes, masks = self.SAM.predict(img_pil, image_pil_bboxes)
+        image_pil_bboxes, index = filter_large_boxes(
+            image_pil_bboxes, w, h, threshold=0.5
+        )
+        masks = masks[index]
+        gdino_conf = gdino_conf[index]
+        ind = np.where(index)[0]
+        phrases = [phrases[i] for i in ind]
+        mask_time = time.time() - start_time
+        return img_pil, masks, image_pil_bboxes, gdino_conf, phrases, mask_time
+
+    def publish_overlay(self, im_label, rgb_frame_stamp, rgb_frame_id):
+        rgb_msg = ros_numpy.msgify(RosImage, im_label, "rgb8")
+        rgb_msg.header.stamp = rgb_frame_stamp
+        rgb_msg.header.frame_id = rgb_frame_id
+        self.image_pub.publish(rgb_msg)
+
+    def publish_mask(self, mask, rgb_frame_stamp, rgb_frame_id):
+        label_msg = ros_numpy.msgify(RosImage, mask.astype(np.uint8), "mono8")
+        label_msg.header.stamp = rgb_frame_stamp
+        label_msg.header.frame_id = rgb_frame_id
+        label_msg.encoding = "mono8"
+        self.mask_pub.publish(label_msg)
+
+    def save_mask(self, filename, img, depth, mask):
+        rgb_path = os.path.join("/tmp", f"{filename}_rgb.jpg")
+        depth_path = os.path.join("/tmp", f"{filename}_depth.png")
+        mask_path = os.path.join("/tmp", f"{filename}_mask.png")
+        cv2.imwrite(depth_path, depth)
+        mask[mask > 0] = 255
+        cv2.imwrite(mask_path, mask)
+        PILImg.fromarray(img).save(rgb_path)
