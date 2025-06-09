6cae70ea7fb3e202db89117dd629050cdfea44f3
jishnu.p@cs260569.utdallas.edu <jishnu.p@utdallas.edu>
2025-06-09 00:14:56 -0500

    mv

--- vie/run_dual_obj_pose_est.sh
+++
@@ -1,9 +0,0 @@
-#----------------------------------------------------------------------------------------------------
-# Work done while being at the Intelligent Robotics and Vision Lab at the University of Texas, Dallas
-# Please check the licenses of the respective works utilized here before using this script.
-# 🖋️ Jishnu Jaykumar Padalunkal (2025).
-#----------------------------------------------------------------------------------------------------
-
-export TOKENIZERS_PARALLELISM=True
-PYTHONWARNINGS="ignore"
-python run_dual_object_pose_estimation.py --task_dir "$1" --demo_frames $2 --rollout_frames $3
